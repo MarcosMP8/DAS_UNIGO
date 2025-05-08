189c375dc11db9649dda1cabd9611c5be77b30b5
EdrochaA <edurochaastutiani@gmail.com>
2025-05-08 10:19:47 +0200

    Link arreglado

--- app/src/main/java/com/example/unigo/network/RetrofitClient.java
+++ app/src/main/java/com/example/unigo/network/RetrofitClient.java
@@ -10,7 +10,7 @@ public class RetrofitClient {
     public static Retrofit getInstance() {
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl("http://ec2-51-44-167-78.eu-west-3.compute.amazonaws.com/erocha002/WEB/unigo")
+                    .baseUrl("http://ec2-51-44-167-78.eu-west-3.compute.amazonaws.com/erocha002/WEB/unigo/")
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
